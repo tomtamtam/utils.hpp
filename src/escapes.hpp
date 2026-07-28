e34bd98483a5a68e7eea29ec0fa36b4f8b073034
tomtamtam <iwan.tom@proton.me>
2026-07-29 01:56:11 +0200

    escapes.hpp -> Colors

---
+++ src/escapes.hpp
@@ -0,0 +1,81 @@
+// Copyright (c) 2026 Tom Iwan
+// PERMISSION IS HEREBY GARANTED TO ANYONE TO DO ANYTHING WITH THIS SHIT EXEPT FOR THE USE IN PROJECTS ACCUMULATING CAPITAL
+// THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <format>
+#include <unordered_map>
+
+namespace Escapes
+{
+	enum Color {
+		BLACK,
+		RED,
+		GREEN,
+		YELLOW,
+		BLUE,
+		MAGENTA,
+		CYAN,
+		WHITE
+	};
+
+	const std::unordered_map<Color, int> COLOR_MAP = {
+		{BLACK,   0},
+		{RED,     1},
+		{GREEN,   2},
+		{YELLOW,  3},
+		{BLUE,    4},
+		{MAGENTA, 5},
+		{CYAN,    6},
+		{WHITE,   7}
+	};
+
+	const int COLOR_OFFSET = 30;
+	const int UNSATTURATED_OFFSET = 90;
+
+	const int COLOR_OFFSET_BASE = 40;
+	const int UNSATTURATED_OFFSET_BASE = 60;
+
+    struct ColorRGB
+    {
+        uint8_t R, G, B;
+    };
+
+    inline ColorRGB RGB(uint8_t r, uint8_t g, uint8_t b) //there because f not in namespace, no brace initialization
+    {
+        return {r, g, b};
+    }
+
+	inline std::string ColoredText(const std::string &s, Color c)
+	{
+		return std::format("\x1B[{}m{}\033[0m", COLOR_OFFSET + COLOR_MAP.at(c), s);
+	}
+
+	inline std::string ColoredTextLight(const std::string &s, Color c)
+	{
+		return std::format("\x1B[{}m{}\033[0m", COLOR_OFFSET + UNSATTURATED_OFFSET + COLOR_MAP.at(c), s);
+	}
+
+    inline std::string ColoredTextRGB(const std::string &s, ColorRGB c)
+    {
+        return std::format("\x1B[38;2;{};{};{}m{}\x1B[0m", c.r, c.g, c.b, s);
+    }
+
+    inline std::string ColoredBase(const std::string &s, Color c)
+    {
+		return std::format("\033[3;{};30m{}\033[0m", COLOR_OFFSET_BASE + COLOR_MAP.at(c), s);
+    }
+
+    inline std::string ColoredBaseLight(const std::string &s, Color c)
+    {
+		return std::format("\033[3;{};30m{}\033[0m", COLOR_OFFSET_BASE + UNSATTURATED_OFFSET_BASE + COLOR_MAP.at(c), s);
+    }
+
+    inline std::string ColoredBaseRGB(const std::string &s, ColorRGB c)
+    {
+        return std::format("\x1B[48;2;{};{};{}m{}\x1B[0m", c.r, c.g, c.b, s);
+    }
+};
